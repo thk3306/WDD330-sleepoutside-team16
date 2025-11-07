7fdddc9e56aea5d1747863e128132674d328caa2
Emil Roding <emilroding27873@gmail.com>
2025-11-07 23:17:45 +1300

    lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
-const element = document.querySelector('.product-list');
+const element = document.querySelector(".product-list");
 
-const productList = new ProductList('all', dataSource, element);
+const productList = new ProductList("all", dataSource, element);
 
 productList.init();
