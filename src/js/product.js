403f26ba2b3ac2098bd1f44dc3862c7b4695e11b
Emil Roding <emilroding27873@gmail.com>
2025-11-07 15:29:06 +1300

    test

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,18 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
+
+
 
 // add to cart button event handler
 async function addToCartHandler(e) {
