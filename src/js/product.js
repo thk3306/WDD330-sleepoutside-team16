9cd22bd161dcbcf123be79b8a18d5dbda7e52e0a
erin.major <erinmajor254@gmail.com>
2025-11-06 20:32:44 -0700

    fix lint and formatting

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,18 @@
-import { getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam('product');
-const dataSource = new ProductData('tents');
+const productId = getParam("product");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
-console.log(dataSource.findProductById(productId));
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
 // Wait for DOM to load before adding listener
 // document.addEventListener('DOMContentLoaded', function() {
@@ -25,4 +23,3 @@ async function addToCartHandler(e) {
 //     console.error("Add to cart button not found");
 //   }
 // });
-
