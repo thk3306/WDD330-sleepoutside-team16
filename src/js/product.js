ed24e94c64281cb4ad6a8a391c831ad5ced610e6
Emil Roding <emilroding27873@gmail.com>
2025-11-07 16:08:21 +1300

    attempt

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,14 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
 const productId = getParam('product');
+const dataSource = new ProductData('tents');
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 console.log(dataSource.findProductById(productId));
-
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
 
 // add to cart button event handler
 async function addToCartHandler(e) {
