15b86da6131adb51c12f067b0294870f79c958d4
Emil Roding <emilroding27873@gmail.com>
2025-11-14 18:30:21 +1300

    run lint & build etc

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,11 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import {loadHeaderFooter, getParam} from "./utils.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const category = getParam('category') || 'tents'; // default to tents if no category specified
-document.getElementById('category-name').textContent = category.charAt(0).toUpperCase() + category.slice(1);
+const category = getParam("category") || "tents"; // default to tents if no category specified
+document.getElementById("category-name").textContent =
+  category.charAt(0).toUpperCase() + category.slice(1);
 
 const dataSource = new ProductData(category);
 const element = document.querySelector(".product-list");
