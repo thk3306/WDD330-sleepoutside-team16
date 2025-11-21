5faf70a9475d3d38473a614f7bc3952dad7d8312
Emil Roding <emilroding27873@gmail.com>
2025-11-21 15:41:04 +1300

    started team activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,8 +3,5 @@ import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
-const cart = new ShoppingCart(
-  "so-cart",
-  document.querySelector(".product-list"),
-);
-cart.init();
+document.querySelector('.checkout-button').addEventListener('click', () => location.href = '/checkout/index.html');
+
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
+
+const cart = new ShoppingCart(
+  "so-cart",
+  document.querySelector(".product-list"),
+);
+cart.init();
+
+
