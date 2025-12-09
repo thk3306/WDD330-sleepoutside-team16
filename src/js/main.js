f55d5f6d3c942765a89e87a5d1a05b0dbccc6987
Emmanuel Nasir <emmanuel.nasi@gmail.com>
2025-12-09 04:53:31 -0800

    Add customizable alert system to index.html
    - Created alert.js module with Alert class
    - Added alerts.json with sample alerts
    - Integrated alert system into main.js
    - Added CSS styling for alert display
    - Alerts are prepended to main element as required

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
+// Import the new Alert system
+import Alert from "./alert.js";
+
+// Load header and footer on page load
 loadHeaderFooter();
+
+
+
+// Initialize alert system for homepage
+// This will load alerts from alerts.json and display them at the top of the page
+const alertSystem = new Alert('/public/json/alerts.json');
+alertSystem.init();
